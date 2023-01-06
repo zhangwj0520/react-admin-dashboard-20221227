d429f1af060c146dc8b9a2e116b0cfd79710aa81
张为杰 <zhangweijie@cestc.cn>
2023-01-06 11:24:17 +0800

    Transactions

--- server/index.js
+++ server/index.js
@@ -16,7 +16,8 @@ import salesRoutes from './routes/sales.js'
 import User from './models/User.js'
 import Product from './models/Product.js'
 import ProductStat from './models/ProductStat.js'
-import { dataUser, dataProduct, dataProductStat } from './data/index.js'
+import Transaction from './models/Transaction.js'
+import { dataUser, dataProduct, dataProductStat, dataTransaction } from './data/index.js'
 
 /* CONFIGURATION */
 dotenv.config()
@@ -51,7 +52,7 @@ mongoose
     // OverallStat.insertMany(dataOverallStat);
     // Product.insertMany(dataProduct)
     // ProductStat.insertMany(dataProductStat)
-    // Transaction.insertMany(dataTransaction);
+    // Transaction.insertMany(dataTransaction)
     // User.insertMany(dataUser)
   })
   .catch((error) => console.log(`${error} did not connect`))
